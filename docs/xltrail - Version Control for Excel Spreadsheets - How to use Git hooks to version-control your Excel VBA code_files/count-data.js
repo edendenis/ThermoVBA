6ed46b976990470fa65294f07a35726a03073352
edendenis <49501168+edendenis@users.noreply.github.com>
2024-02-17 03:56:28 -0300

    Adding file(s)
    Adding file(s).

---
+++ docs/xltrail - Version Control for Excel Spreadsheets - How to use Git hooks to version-control your Excel VBA code_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"https:\/\/www.xltrail.com\/blog\/auto-export-vba-commit-hook","comments":44}]});
+}
